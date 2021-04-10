276a7b9d75c942c3d0d686e24da8cf6db6a7f1f5
Chinedu Orie <nedsoftdeveloper@gmail.com>
2021-04-10 13:30:04 +0100

    chore: add consultation hours to physician

--- src/app/controllers/Physician.java
+++ src/app/controllers/Physician.java
@@ -17,10 +17,49 @@ import java.util.Random;
 public class Physician extends Personnel {
 
     private String[] expertises;
+    private int consultationMinutesLeft;
+    private String consultationHours;
 
-    public Physician(int id, String fn, String adr, String tel, String[] expertises) {
+    public Physician(int id, String fn, String adr, String tel, String[] expertises, String cHour) {
         super(id, fn, adr, tel);
         this.setExpertises(expertises);
+        consultationHours = cHour;
+        consultationMinutesLeft = 120;
+    }
+
+    /**
+     * @return the isFullyBooked
+     */
+    public boolean isFullyBooked() {
+        return consultationMinutesLeft == 0;
+    }
+
+    /**
+     * @return the consultationMinutesLeft
+     */
+    public int getConsultationMinutesLeft() {
+        return consultationMinutesLeft;
+    }
+
+    /**
+     * @param consultationMinutesLeft the consultationMinutesLeft to set
+     */
+    public void setConsultationMinutesLeft(int consultationMinutesLeft) {
+        this.consultationMinutesLeft = consultationMinutesLeft;
+    }
+
+    /**
+     * @return the consultationHour
+     */
+    public String getConsultationHours() {
+        return consultationHours;
+    }
+
+    /**
+     * @param consultationHour the consultationHour to set
+     */
+    public void setConsultationHours(String consultationHour) {
+        this.consultationHours = consultationHour;
     }
 
     /**
@@ -33,46 +72,45 @@ public class Physician extends Personnel {
     /**
      * Assign expertise to this physician
      */
-    public void setExpertises(String [] exps) {
+    public void setExpertises(String[] exps) {
         expertises = exps;
     }
 
-    public static Physician create(int id, String fn, String adr, String tel, String[] expertises) {
-        Physician physician = new Physician(id, fn, adr, tel, expertises);
+    public static Physician create(int id, String fn, String adr, String tel, String[] expertises, String cHour) {
+        Physician physician = new Physician(id, fn, adr, tel, expertises, cHour);
         PhysicianModel.save(physician);
         return physician;
     }
-    
-   public ArrayList<Treatment> getTreatments() {
-     ArrayList<Treatment> matches = new ArrayList<Treatment>();
-     ArrayList<Treatment> trs = TreatmentModel.all();
-     
-     for (Treatment tr: trs){
-       if(tr.getPhysicianId() == this.getId()){
-          matches.add(tr);
-       }
-     }
-     return matches;
-   }
-   
+
+    public ArrayList<Treatment> getTreatments() {
+        ArrayList<Treatment> matches = new ArrayList<Treatment>();
+        ArrayList<Treatment> trs = TreatmentModel.all();
+
+        for (Treatment tr : trs) {
+            if (tr.getPhysicianId() == this.getId()) {
+                matches.add(tr);
+            }
+        }
+        return matches;
+    }
+
     public ArrayList<Treatment> getTreatments(String status) {
-     ArrayList<Treatment> matches = new ArrayList<Treatment>();
-     ArrayList<Treatment> trs = TreatmentModel.all();
-     for (Treatment tr: trs){
-       if(tr.getPhysicianId() == this.getId() && tr.getStatus().equalsIgnoreCase(status)){
-          matches.add(tr);
-       }
-     }
-     return matches;
-   }
+        ArrayList<Treatment> matches = new ArrayList<Treatment>();
+        ArrayList<Treatment> trs = TreatmentModel.all();
+        for (Treatment tr : trs) {
+            if (tr.getPhysicianId() == this.getId() && tr.getStatus().equalsIgnoreCase(status)) {
+                matches.add(tr);
+            }
+        }
+        return matches;
+    }
 
     public String toString() {
         String exps = "";
-        for(String exp: expertises){
-            exps = exps+exp+"\n";
+        for (String exp : expertises) {
+            exps = exps + exp + "\n";
         }
-        return "ID: " + this.getId() + "\n" + "Full name: " + this.getFullName() + "\n" + "Address: " + this.getAddress() + "\n" + "Telephone: " + this.getTelephone() + "\n"+"=====Areas of Expertise====\n" + exps;
+        return "ID: " + this.getId() + "\n" + "Full name: " + this.getFullName() + "\n" + "Address: " + this.getAddress() + "\n" + "Telephone: " + this.getTelephone() + "\nConsulation Hours: " + this.getConsultationHours() + "\n=====Areas of Expertise====\n" + exps;
     }
-    
- 
+
 }
--- src/app/models/PhysicianModel.java
+++ src/app/models/PhysicianModel.java
@@ -76,11 +76,11 @@ public class PhysicianModel {
 
     public static void seed() {
 
-        Physician.create(1, "Dr. Ashly Tatnell", "978 Monica Circle", "+690-500-3079", new String[]{"Physiotherapy", "Rehabilitation"});
-        Physician.create(2, "Dr. Wye Brockwell", "71906 Waywood Circle", "+790-994-9953", new String[]{"Osteopathy", "Rehabilitation"});
-        Physician.create(3, "Dr.Cybil Everix", "41 Russell Pass", "+930-229-2270", new String[]{"Physiotherapy", "Osteopathy", "Rehabilitation"});
-        Physician.create(4, "Jeddy Strongel", "79818 Transport Hill", "+885-418-8711", new String[]{"Rehabilitation"});
-        Physician.create(5, "Dr. Joana Terren", "39 Muir Circle", "+224-521-3171", new String[]{"Physiotherapy", "Osteopathy"});
+        Physician.create(1, "Dr. Ashly Tatnell", "978 Monica Circle", "+690-500-3079", new String[]{"Physiotherapy", "Rehabilitation"}, "Monday 1-3pm");
+        Physician.create(2, "Dr. Wye Brockwell", "71906 Waywood Circle", "+790-994-9953", new String[]{"Osteopathy", "Rehabilitation"}, "Wednesday 2-4pm");
+        Physician.create(3, "Dr.Cybil Everix", "41 Russell Pass", "+930-229-2270", new String[]{"Physiotherapy", "Osteopathy", "Rehabilitation"}, "Tuesday 3-5pm");
+        Physician.create(4, "Jeddy Strongel", "79818 Transport Hill", "+885-418-8711", new String[]{"Rehabilitation"}, "Saturday 4-6pm");
+        Physician.create(5, "Dr. Joana Terren", "39 Muir Circle", "+224-521-3171", new String[]{"Physiotherapy", "Osteopathy"}, "Friday 2-4pm");
 
     }
 
